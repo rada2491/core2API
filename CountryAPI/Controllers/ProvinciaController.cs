1b64410896f3c7f6baae1a5e641bc8fa587ffde3
Carlos <cbr2491@gmail.com>
2018-08-23 16:48:40 -0600

    first commit

---
+++ CountryAPI/Controllers/ProvinciaController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CountryAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CountryAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Pais/{PaisId}/Provincia")]
+    public class ProvinciaController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProvinciaController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Provincia> GetAll(int PaisId)
+        {
+            return context.Provincias.Where(x => x.PaisId == PaisId).ToList();
+        }
+
+        [HttpGet("{id}", Name = "provinciaById")]
+        public IActionResult GetById(int id)
+        {
+            var provincia = context.Provincias.FirstOrDefault(x => x.Id == id);
+
+            if (provincia == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(provincia);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Provincia pro, int PaisId)
+        {
+            pro.PaisId = PaisId;
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            context.Provincias.Add(pro);
+            context.SaveChanges();
+
+            return new CreatedAtRouteResult("provinciaById", new { id = pro.Id }, pro);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Provincia pro, int id)
+        {
+            if (pro.Id != id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(pro).State = EntityState.Modified;    
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pro = context.Provincias.FirstOrDefault(x => x.Id == id);
+            if (pro == null)
+            {
+                return NotFound();
+            }
+
+            context.Provincias.Remove(pro);
+            context.SaveChanges();
+            return Ok(pro);
+        }
+    }
+}
---
+++ CountryAPI/Startup.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CountryAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace CountryAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<Models.ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer();
+
+            services.AddMvc().AddJsonOptions(ConfigureJson);
+        }
+
+        private void ConfigureJson(MvcJsonOptions obj)
+        {
+            obj.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+
+            if (!context.Paises.Any())
+            {
+                context.Paises.AddRange(new List<Pais>()
+                {
+                    new Pais() {Nombre = "Costa Rica"
+                    },
+                    new Pais() {Nombre = "Rusia",  Provincias = new List<Provincia>(){
+                        new Provincia(){Nombre = "Vodka"},
+                        new Provincia() {Nombre = "Ron" }
+                    }
+                    },
+                    new Pais() {Nombre = "Guatemala",  Provincias = new List<Provincia>(){
+                        new Provincia(){Nombre = "Guatemala"},
+                        new Provincia(){Nombre = "Antigua Guatemala"} } }
+                });
+
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
